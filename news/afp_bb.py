8d23f20430e10e1350389ff07d202ef7fb6672f9
mayonnaises <ernani.184404@gmail.com>
2021-06-06 18:27:47 +0900

    Update afp_bb.py

--- news/afp_bb.py
+++ news/afp_bb.py
@@ -33,4 +33,35 @@ Execution example:
 
 
 class AFPBBNews:
-    pass
+
+    def __init__(self):
+        self.response = requests.get(
+            'https://www.afpbb.com/subcategory/theme-corona')
+        self.title_dict = {}
+
+    def __iter__(self):
+        parse_result = BeautifulSoup(self.response.text, 'html.parser')
+
+        for parse in parse_result.select('a.block.move'):
+            # Get news title and url
+            yield [
+                parse.find('h3').get_text(strip=True), parse['href']]
+
+    def get_titles(self):
+        '''
+        key: News number
+        value: List news title and url
+        '''
+        self.title_dict = {
+            index: titles for index, titles in enumerate(self.__iter__(), 1)}
+
+    def display_titles(self):
+        for index, title in self.title_dict.items():
+            print(f'\n{index} : {title[0]}')
+
+
+
+if __name__ == '__main__':
+    afp = AFPBBNews()
+    afp.get_titles()
+    afp.display_titles()
