f1e4fb1cea369fa4ca86afbc0751904ada4167bd
mayonnaises <ernani.184404@gmail.com>
2021-06-24 06:54:14 +0900

    Update reuters.py

--- news/reuters.py
+++ news/reuters.py
@@ -33,3 +33,36 @@ Execution example:
               .
               .
 '''
+
+
+class REUTERS:
+
+    def __init__(self):
+        self.response = requests.get(
+            'https://jp.reuters.com/search/news?blob=コロナウイルス')
+        self.title_dict = {}
+
+    def __iter__(self):
+        parse_result = BeautifulSoup(self.response.text, 'html.parser')
+
+        for parse in parse_result.select('h3.search-result-title'):
+            # Get news title and url
+            yield [parse.get_text(strip=True), parse.find('a')['href']]
+
+    def get_titles(self):
+        '''
+        key: News number
+        value: List news title and url
+        '''
+        self.title_dict = {
+            index: titles for index, titles in enumerate(self.__iter__(), 1)}
+
+    def display_titles(self):
+        for index, title in self.title_dict.items():
+            print(f'\n{index} : {title[0]}')
+
+
+if __name__ == '__main__':
+    reuters = REUTERS()
+    reuters.get_titles()
+    reuters.display_titles()
